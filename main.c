d5d594f12ec0ff74af449528a8874704b3856b0b
Daniel Machado <danielmachadopintos@gmail.com>
2018-08-30 09:06:34 -0300

    atualizando para entendimetno v2
    Reestruturação do código

--- main.c
+++ main.c
@@ -3,7 +3,8 @@
 #include <locale.h>
 
 /*
-### Tabela Simbolo:
+TODO: https://gitlab.tadsufpr.net.br/ds141-alexkutzke/material/blob/master/aula_04.md
+HACK: Tabela Simbolo:
   * Para comparar duas chaves a e b.
   * put() e get().
 */
@@ -20,27 +21,11 @@ typedef struct tabela{
   time_te valor;
 } timetable_t;
 
-typedef struct pilha{
-  int topo;
-  int tam;
-  timetable_t *vetor;
-} Pilha;
-
-Pilha * create_stack(int tam);
-void push(Pilha *pilha, int e);
-int full(Pilha *p);
-// int pop(Pilha *p);
-int empty(Pilha *p);
-void free_stack(Pilha *p);
-int time_cmp(time_t h1, time_t h2);
-time_te * carregaVetor(int horas, int minutos, int segundos);
-
 int main(int argc, char const *argv[]) {
   setlocale(LC_ALL, "Portuguese");
   int tam = 3;
   Pilha *p = create_stack(tam+1);
   time_te *valor = carregaVetor(10,9,2);
-  char *nome = (char *) malloc(sizeof(c));
   push(p, valor);
 
   // while (!empty(p)) {
@@ -59,7 +44,9 @@ time_te * carregaVetor(int horas, int minutos, int segundos){
   return elemento;
 }
 
-/* Retorna:
+/*
+Função: comparar os tempos fornecidos.
+Retorna:
  1  se h1 >  h2
  0  se h1 == h2
  -1 se h1 <  h2 */
@@ -67,44 +54,29 @@ int time_cmp(time_t h1, time_t h2){
   return 1;
 }
 
-/* Criando a pilha */
-Pilha * create_stack(int tam){
-  Pilha *p = (Pilha *) malloc(sizeof(Pilha));
-  p->topo = 0;
-  p->tam = tam;
-  p->vetor = (timetable_t *) malloc(tam*sizeof(Pilha));
-  return p;
-}
-
-int full(Pilha *p){
-  return (p->topo == (p->tam-1));
-}
-
 void push(Pilha *pilha, time_te *key, char *value){
-  if(full(pilha)){
-    printf("%s\n","pilha cheia: capacidade ultrapassada");
+  int i;
+  // procura pela proxima posição livre
+  while(pilha[i++]){
+    if (pilha[i] == 0) {
+      pilha[i]->valor = key;
+      pilha[i]->chave = value;
+    }
+  }
+}
+
+char * pop(Pilha *p){
+  if(empty(p)){
+    printf("%s\n","Lista está vazia");
     exit(1);
   }
-  pilha->vetor[pilha->topo]->valor = key;
-  pilha->vetor[pilha->topo]->chave = value;
-  pilha->topo++;
+  p->topo--;
+  char *v = p->vetor[p->topo]->chave;
+  return v;
 }
-
-// char * pop(Pilha *p){
-//   if(empty(p)){
-//     printf("%s\n","Lista está vazia");
-//     exit(1);
-//   }
-//   p->topo--;
-//   char *v = p->vetor[p->topo]->chave;
-//   return v;
-// }
 
 int empty(Pilha *p){
-  return (p->topo == 0);
-}
+  for (int i=0;i < count; i++) {
 
-void free_stack(Pilha *p){
-  free(p->vetor);
-  free(p);
+  }
 }
